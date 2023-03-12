dcf3c6de0fb0dd8e7a2c99f828fd42490b8e4aaf
Achraf Mataich <achrafmataich@users.noreply.github.com>
2023-03-12 15:33:09 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # menu-builder-console
+## About the package
+This is a simple menu builder for java console apps that create a navigable menu
+## Usage
+Create the whole logic in the instanciation if the menu
+
+The very simple way to make the first menu with only one menu item that does nothing would be:
+
+```JAVA
+Menu menu = new Menu("Main Menu", new ArrayList<Menu.MenuItem>(){{
+    add(new Menu.MenuItem("1", "Say Hello", null, null));
+}});
+
+menu.showMenu();
+```
---
+++ lib/src/main/java/com/outsiderninjadevs/menubuilderconsole/Menu.java
@@ -0,0 +1,127 @@
+package com.outsiderninjadevs.menubuilderconsole;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Menu {
+
+    private String title;
+    private ArrayList<MenuItem> menuItems;
+    private Menu supMenu = null;
+
+    public Menu(String title, ArrayList<MenuItem> menuItems) {
+        this.title = title;
+        this.menuItems = menuItems;
+        for (Menu.MenuItem menuItem : this.menuItems) {
+            if (menuItem.getSubMenu() != null) {
+                menuItem.getSubMenu().setSupMenu(this);
+            }
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setMenuItems(ArrayList<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public Menu getSupMenu() {
+        return supMenu;
+    }
+
+    public void setSupMenu(Menu supMenu) {
+        this.supMenu = supMenu;
+    }
+
+    public String getFullPathMenuTitle() {
+        return ((this.supMenu != null) ? this.supMenu.getFullPathMenuTitle() + " > " : "") + "[" + this.title + "]";
+    }
+
+    public void showMenu() {
+        Scanner sc = new Scanner(System.in);
+        String choice = null;
+        do {
+            System.out.println(this.getFullPathMenuTitle());
+            for (MenuItem menuItem : this.menuItems) {
+                System.out.println("#" + menuItem.getKey() + "\t" + menuItem.getText());
+            }
+            System.out.println("#q\tQuit");
+            System.out.print("? : ");
+            choice = sc.nextLine();
+            for (MenuItem menuItem : this.menuItems) {
+                if (menuItem.getKey().equals(choice)) {
+                    if (menuItem.getSubMenu() != null) {
+                        menuItem.getSubMenu().showMenu();
+                        System.out.println("OUTOUTOUTOUT");
+                    } else if(menuItem.getF() != null) {
+                        menuItem.getF().run();
+                    } else {
+                        System.out.println("!!ERROR");
+                    }
+                }
+            }
+        } while (!choice.equals("q"));
+    }
+
+    public static class MenuItem {
+        private String key;
+        private String text;
+        private Menu subMenu;
+        private Runnable f;
+
+        MenuItem(String key, String text, Menu subMenu, Runnable f) {
+            this.key = key;
+            this.text = text;
+            this.subMenu = subMenu;
+            this.f = f;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public Menu getSubMenu() {
+            return subMenu;
+        }
+
+        public void setSubMenu(Menu subMenu) {
+            this.subMenu = subMenu;
+        }
+
+        public Runnable getF() {
+            return f;
+        }
+
+        public void setF(Runnable f) {
+            this.f = f;
+        }
+    }
+
+    public boolean test(){
+        return true;
+    }
+}
+
